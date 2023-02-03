4dfeb07461ffeaba0dd5c9d328b2bd4d412d13f3
Tati-Zhurr <zhtv88@gmail.com>
2023-02-03 13:13:02 +0100

    feat: change server.js add db and dbAuthUsers

--- server.js
+++ server.js
@@ -3,13 +3,118 @@ const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
 
+const db =  {
+  category: [
+    {
+      id: 1,
+      name: 'Название модуля',
+      lessons: [], // ID уроков этой категории
+      tests: [], // ID тестов этой категории
+      tasks: [] // ID задач этой категории
+    },
+  ],
+  lessons: [
+    {
+      id: 1,
+      name: 'Название урока',
+      content: [
+        {
+          tag: 'Название тега',
+          class: ['Название класса'],
+          text: 'Текст урока'
+        },        
+      ],
+      links: {
+        prev: 'link', // ID предыдущего урока
+        next: 'link', // ID следующего урока
+        test: 'link', // ID теста к этому уроку
+        task: 'link' // ID задачи к этому уроку
+      }
+    },
+  ],
+  tests: [
+    {
+      id: 1,
+      name: 'Название теста',
+      questions: [
+        {
+          id: 1,
+          question: 'Текст вопроса',
+          answers: ['Текст вариантов ответа'],
+          rightAnswer: ['Текст правильного ответа'] // Может быть один или несколько
+        },        
+      ],
+      links: {
+        prev: 'link', // ID предыдущего теста
+        next: 'link', // ID следующего теста
+        lesson: 'link', // ID урока к этому тесту
+        task: 'link' // ID задачи к этому тесту
+      }
+    },
+  ],
+  tasks: [
+    {
+      id: 1,
+      name: 'Название задачи',
+      content: [
+        {
+          tag: 'Название тега',
+          class: ['Название класса'],
+          text: 'Текст урока'
+        },        
+      ],
+      links: {
+        prevTaskBlock: 'link', // ID предыдущего блока задач
+        nextTaskBlock: 'link', // ID следующего следующего
+        test: 'link', // ID теста к этому блоку задач
+        lesson: 'link', // ID урока к этому блоку задач
+        nextLesson: 'link', // ID следующего урока
+        prevLesson: 'link' // ID следующего урока
+      }
+    },
+  ],
+  comments: [
+    {
+      id: 1,
+      lesson: 1, // ID урока, к которому относится комментарий
+      user: 1, // ID пользователя, который написал комментарий
+      content: 'Текст комментария'
+    }
+  ],
+  
+}
+
+
+const dbAuthUsers =  {
+  users: [
+    {
+      id: 1,
+      email: 'email пользователя',
+      password: 'Пароль пользователя',
+      name: 'Имя пользователя',
+      done: {
+        lessons: [], // ID уроков, которые прошел пользователь
+        tests: [{
+          id: 1, // ID пройденного теста
+          result: 50 // последний результат
+        }],
+        tasks: [] // ID задач, которые решил пользователь
+      }
+    }
+  ]
+}
+
+
 const server = jsonServer.create()
-const router = jsonServer.router('./database.json')
+const router = jsonServer.router(dbAuthUsers)
+const router2 = jsonServer.router(db)
 const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(jsonServer.defaults());
+
+server.use(router2)
 
 const SECRET_KEY = '123456789'
 
@@ -93,9 +198,7 @@ server.post('/auth/login', (req, res) => {
 
 server.use(/^(?!\/auth).*$/,  (req, res, next) => {
   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({status, message})
+    server.use(router2)
     return
   }
   try {
